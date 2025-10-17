b18f3ab2cd8fb41ba57611699a86c559489cf78a
Abhinav Rana <abhinavrana@Abhinavs-MacBook-Air.local>
2025-10-17 22:39:13 +1300

    Added page speed tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Header from "@/components/header-server"
 import Footer from "@/components/footer"
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
+          <SpeedInsights />
           <Footer />
         </ThemeProvider>
       </body>
